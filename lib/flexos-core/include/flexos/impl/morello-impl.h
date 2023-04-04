84e64b7032ce4d5b68a4a9a4aa55225a94aa971d
John Kressel <jkressel.apps@gmail.com>
2023-04-04 16:20:24 +0000

    Stack coherence fixes for 1 arg+ret, DANGER there be dragons elsewhere!

--- lib/flexos-core/include/flexos/impl/morello-impl.h
+++ lib/flexos-core/include/flexos/impl/morello-impl.h
@@ -663,121 +663,90 @@ __asm__ volatile (	\
 	"stp c25, c26, [sp, #-32]!\n"		\
 	"stp c27, c28, [sp, #-32]!\n"		\
 	"stp c29, c30, [sp, #-32]!\n"		\
-	:								\
-	:								\
-	: "sp"							\
-);								\
-	\
-	\
-	\
 /* x12 will hold tsb sp and x13 will hold tsb fp */ 	\
-__asm__ volatile(	\
 	"mov x10, %0\n"	\
 	"mul x11, x10, %1\n"	\
 	"add x11, x11, %2\n"	\
 	"ldp x12, x13, [x11]\n"	\
 	"stp x12, x13, [sp, #-16]!\n"	\
-	:				\
-	: "r"(uk_thread_get_tid()), "r" (sizeof(struct uk_thread_status_block)), "r" ((tsb_comp ## key_from))	\
-	: "x10", "x11", "x12", "x13", "sp"	\
-);	\
-	\
-	\
-	\
-/* backup the current sp and fp */ 	\
+	/* backup the current sp and fp */ 	\
 /*tsb_comp ## key_from[tid].sp = register asm("sp");*/	\
 /*tsb_comp ## key_from[tid].bp = register asm("fp");*/	\
 /* x11 hold the base of tsb_comp ## key_from as calculated above */ 	\
-\
-__asm__ volatile(	\
 	"mov x10, sp\n"	\
 /*This is to allow us to store things like ddc, return address */	\
 	"sub x10, x10, #32\n"	\ 
 	"stp x10, fp, [x11]\n"	\
-	:		\
-	: 	\
-	: "x10", "memory"	\
-);	\
-	\
-	\
-/* Now we need to load the dest compartment id into a register and the number of arguments*/	\
+	/* Now we need to load the dest compartment id into a register and the number of arguments*/	\
 /* TODO: tying to load the address of tsb comp for the target may not work, may need to revist this in the future*/	\
-__asm__ volatile(	\
-	"mov x10, %0\n"	\
-	"mov x9, %1\n"	\
-	"mov x11, %2\n"	\
-	"mov x12, %3\n"	\
-	"mov x13, %4\n"	\
-	:				\
-	: "i"(key_to), "i"(1), "r"(f_ptr), "r"(tsb_comp ## key_to), "r"(uk_thread_get_tid())	\
-	: "x9", "x10", "x11", "x12", "x13"	\
-);	\
+	"mov x10, %3\n"	\
+	"mov x9, %4\n"	\
+	"mov x11, %5\n"	\
+	"mov x12, %6\n"	\
+	"mov x13, %0\n"	\
+	\
+	\
+	"mov x0, %7\n"	\
 	\
 	\
 	\
-/* Okay now we need to sort out the args, they need to go in c0-c7 */	\
-/* In this case we only have one argument so only need to use c0 */ 	\
-	if (IS_CAP(arg1)) {	\
-		flexos_morello_move_arg_cap_into_reg(arg1, 1);	\
-	} else {	\
-		flexos_morello_move_arg_int_into_reg(arg1, 1);	\
-	}	\
-	\
-	\
-	\
-/* Load the switcher caps and branch to switcher using unsealing instruction ldpblr */	\
-__asm__ volatile(	\
-	"ldr c14, [%0]\n"	\
+	/* Load the switcher caps and branch to switcher using unsealing instruction ldpblr */	\
+	"ldr c14, [%8]\n"	\
 	"ldpblr c29, [c14]\n" \
 	:				\
-	: "r"((uintptr_t *)(&(switcher_call_comp ## key_from)))	\
-	: "x11", "c29", "c30", "x9", "x8", "x7", "x6", "x5", "x4", "x3", "x2", "x1", "x0", "x10", "x12", "x13", "x14", "x16", "x17", "x18", "x19", "x20", "x21", "x22", "x23", "x24", "x25", "x26", "x27", "x28", "x31"	\
+	: "r"(uk_thread_get_tid()), "r" (sizeof(struct uk_thread_status_block)), "r" ((tsb_comp ## key_from)), "i"(key_to),	"i"(1), "r"(f_ptr), "r"(tsb_comp ## key_to), "r"(arg1), "r"((uintptr_t *)(&(switcher_call_comp ## key_from)))\
+	: "x10", "x11", "x12", "x13", "sp", "x9", "x0"	\
 );	\
 \
 \
 \
-__asm__ volatile (	\
+\
+\
+__asm__ (	\
+	"nop\n"\
+	:\
+	:\
+	:"x1", "x2", "x3", "x4", "x5", "x6", "x7", "x8", "x9", "x10", "x11", "x12", "x13", "x14", "x15", "x16", "x17", "x18", "x19", "x20", "x21", "x22", "x23", "x24", "x25", "x26", "x27", "x28", "x30"\
+);\
+__asm__ volatile(	\
 	"mov x10, %0\n"\
 	"msr ddc, c29\n"	\
-	:	\
-	:	"r"(uk_thread_get_tid())	\
-	: "x0", "x1", "x2", "x3", "x4", "x5", "x6", "x7", "x8", "x10"	\
-);	\
-\
-\
-\
-/* Need to do reentry */	\
+	\
+	\
+	\
+	/* Need to do reentry */	\
 /* - Restore stack and frame and tsb*/	\
 /* - Pop registers saved */	\
 /* restore tsb */ 	\
-__asm__ volatile(	\
-	"mul x11, x10, %0\n"	\
-	"add x11, x11, %1\n"	\
+	"mul x11, x10, %1\n"	\
+	"add x11, x11, %2\n"	\
 	"ldp x12, fp, [x11]\n"	\
 	"mov sp, x12\n"	\
 	"ldp x12, x13, [sp, #32]!\n"	\
 	"add sp, sp, #16\n"	\
 	"stp x12, x13, [x11]\n"\
-	"add sp, sp, #256\n"	\
-	:				\
-	: "r" (sizeof(struct uk_thread_status_block)), "r" ((tsb_comp ## key_from))	\
-	: "x10", "x11", "x12", "x13", "sp", "fp", "x0", "x1", "x2", "x3", "x4", "x5", "x6", "x7", "x8"	\
-);	\
+	"mov x17, sp\n"	\
+	"add sp, sp, #448\n"	\
+	:\
+	:"r"(uk_thread_get_tid()), "r" (sizeof(struct uk_thread_status_block)), "r" ((tsb_comp ## key_from))\
+	:"x0", "x8", "x10", "x11", "x12", "x13", "x16", "x18", "x17", "x14", "x15", "x19", "x20", "x21", "x22", "x23", "x24", "x25", "x26", "x27", "x28", "x30"\
+);\
+__asm__ (	\
+	"nop\n"\
+	:\
+	:\
+	:"x0", "x1", "x2", "x3", "x4", "x5", "x6", "x7", "x8", "x9", "x10", "x11", "x12", "x13", "x14", "x15", "x16", "x17", "x18", "x19", "x20", "x21", "x22", "x23", "x24", "x25", "x26", "x27", "x28", "x30"\
+);\
+__asm__ (\
 	\
 	\
 	\
-__asm__ volatile (	\
 	"mov x11, %0\n"\
 	"str x0, [x11]\n"	\
-	"sub sp, sp, #256\n"	\
-	:	\
-	:	"r"(&retval_ptr)	\
-	: "x0", "x11", "x10"	\
-);	\
-\
-\
-\
-__asm__ volatile(	\
+	"mov sp, x17\n"	\
+	\
+	\
+	\
 	"ldp c29, c30, [sp], #32\n"		\
 	"ldp c27, c28, [sp], #32\n"		\
 	"ldp c25, c26, [sp], #32\n"		\
@@ -792,11 +761,19 @@ __asm__ volatile(	\
 	"ldp c12, c13, [sp], #32\n"	\
 	"ldp c10, c11, [sp], #32\n"	\
 	"ldp c9, c8, [sp], #32\n"	\
-	:								\
-	:								\
-	: "c18", "c17", "c16", "c15", "c14", "c13",	\
-	 "c12", "c11", "c10", "c9", "c0", "c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8", "c19", "c20", "c21", "c22", "c23", "c24", "c25", "c26", "c27", "c28", "c29", "c30", "sp"							\
+	:	\
+	:	"r"(&retval_ptr)	\
+	: "x0", "x8", "x10", "x11", "x12", "x13", "x16", "x18", "x17", "x14", "x15", "x19", "x20", "x21", "x22", "x23", "x24", "x25", "x26", "x27", "x28", "x30"	\
 );	\
+__asm__ (	\
+	"nop\n"\
+	:\
+	:\
+	:"x0", "x1", "x2", "x3", "x4", "x5", "x6", "x7", "x8", "x9", "x10", "x11", "x12", "x13", "x14", "x15", "x16", "x17", "x18", "x19", "x20", "x21", "x22", "x23", "x24", "x25", "x26", "x27", "x28", "x30"\
+);\
+\
+\
+\
 } while (0)
 
 
